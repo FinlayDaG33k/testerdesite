aa7e4797ef6bfaa73151f3116adf4183af17efeb
Aroop 'Finlay' Roelofs <me@finlaydag33k.nl>
2016-06-06 12:38:54 +0200

    Update Eenzaamheid.php
    Probeer maar eens te kijken wat ik gedaan heb ;)

--- Eenzaamheid.php
+++ Eenzaamheid.php
@@ -3,12 +3,11 @@
 
 <head>
 <title>Eenzaamheid</title>
-
 </head>
 
 <body>
 <h2> Eenzaamheid's formulier</h2>
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>"  method="post">
+<form action="<?= htmlspecialchars($_SERVER['PHP_SELF']);?>"  method="POST">
 Naam: <input type="text" name="Naam" placeholder="John"><br>
 Je gevoelens: <input type="text" name="Gevoel" placeholder="Ik voel me goed"><br>
 De 'ware': <input type="text" name="Ware" placeholder="Lorenzo"><br>
@@ -18,38 +17,21 @@ Uw probleem: <input type="text" name="Probleem" placeholder="Mijn probleem is da
 
 <?php 
 
-
 $Naam = htmlentities($_POST['Naam']);
 $Gevoel = htmlentities($_POST['Gevoel']);
 $Ware = htmlentities($_POST['Ware']);
 $Probleem = htmlentities($_POST['Probleem']);
 
-
-if(empty($_POST['Naam']) AND empty($_POST['Gevoel']) AND empty($_POST['Ware']) AND empty($_POST['Probleem'])) {
-	
-	echo "U heeft niks ingevuld!";
+if(empty($Naam) && empty($Gevoel) && empty($Ware) && empty($Probleem)) {
+	?>
+	U heeft niks ingevuld!
+	<?php
 } else {
-	
-	
-echo "Hallo $Naam Jouw Gevoel $Gevoel dus. Is niet best  je bent verliefd/ er zeker op dat $Ware de Ware is maar je bent er niet helemaal zeker van. Jouw probleem $Probleem dus is ook niet best voor dit allemaal zou ik een psycholoog bezoeken.
-<br> Ik hoop dat dit je heeft geholpen." ;
+	?>
+Hallo, <?= $Naam ?> Jouw Gevoel <?= $Gevoel ?> dus. Is niet best  je bent verliefd/ er zeker op dat <?= $Ware ?> de Ware is maar je bent er niet helemaal zeker van. Jouw probleem <?= $Probleem ?> dus is ook niet best voor dit allemaal zou ik een psycholoog bezoeken.
+<br> Ik hoop dat dit je heeft geholpen.
+<?php
 }
-
-
 ?>
-
 </body>
-
-
-
-
-
-
-
-
-
-
-
-
-
 </html>
